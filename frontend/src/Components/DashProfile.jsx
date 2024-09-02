cf1246fd764acef8e36941ea303a2a3e4ef48f47
Sohom Ray Mandal <sohomray17@gmail.com>
2024-09-02 13:53:48 +0530

    Edit Profile Page UI

--- frontend/src/Components/DashProfile.jsx
+++ frontend/src/Components/DashProfile.jsx
@@ -18,7 +18,7 @@ export default function DashProfile() {
                 Update
             </Button>
         </form>
-        <div className="text-red-500 flex justify-between my-5">
+        <div className="text-red-500 flex justify-between mt-5">
             <span className='cursor-pointer'>Delete Account</span>
             <span className='cursor-pointer'>Sign Out</span>
         </div>
