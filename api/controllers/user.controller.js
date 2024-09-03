c343c17b7931ad5c3f24408cd51f6c3cafc670bc
Sohom Ray Mandal <sohomray17@gmail.com>
2024-09-04 04:23:44 +0530

    Add Signout Functionality

--- api/controllers/user.controller.js
+++ api/controllers/user.controller.js
@@ -63,6 +63,12 @@ export const deleteUser = async (req, res, next) => {
     } catch (error) {
         next(error);
     }
-    
+};
 
+export const signout = (req, res, next) => {
+    try {
+        res.clearCookie('access_token').status(200).json('User Has Been Signed Out');
+    } catch (error) {
+        next(error);
+    }
 }
--- frontend/src/Components/DashProfile.jsx
+++ frontend/src/Components/DashProfile.jsx
@@ -5,7 +5,7 @@ import { getDownloadURL, getStorage, ref, uploadBytesResumable } from 'firebase/
 import { app } from '../firebase';
 import { CircularProgressbar } from 'react-circular-progressbar';
 import 'react-circular-progressbar/dist/styles.css';
-import { updateStart, updateSuccess, updateFailure, deleteUserStart, deleteUserSuccess, deleteUserFailure } from '../Redux/user/userSlice';
+import { updateStart, updateSuccess, updateFailure, deleteUserStart, deleteUserSuccess, deleteUserFailure, signoutSuccess } from '../Redux/user/userSlice';
 import { useDispatch } from 'react-redux';
 import { HiOutlineExclamationCircle } from 'react-icons/hi'
 
@@ -124,7 +124,23 @@ export default function DashProfile() {
             dispatch(deleteUserFailure(error.message));
         }
     };
-    
+
+    const handleSignout = async () => {
+        try {
+            const res = await fetch('/api/user/signout', {
+                method:'POST',
+            });
+            const data = await res.json();
+            if(!res.ok) {
+                console.log(data.message);
+            }
+            else {
+                dispatch(signoutSuccess());
+            }
+        } catch (error) {
+            console.log(error.message);
+        }
+    };
 
   return (
     <div className='max-w-lg mx-auto p-3 w-full'>
@@ -162,7 +178,7 @@ export default function DashProfile() {
         </form>
         <div className="text-red-500 flex justify-between mt-5">
             <span onClick={() => setShowModal(true)} className='cursor-pointer'>Delete Account</span>
-            <span className='cursor-pointer'>Sign Out</span>
+            <span onClick={handleSignout} className='cursor-pointer'>Sign Out</span>
         </div>
         {updateUserSuccess && (
             <Alert color='success' className='mt-5'>
