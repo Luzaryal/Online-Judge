ef6e95ea9dbfa0e09cd30a25a29e1c5d540dc228
Sohom Ray Mandal <sohomray17@gmail.com>
2024-09-04 16:13:11 +0530

    Complete Create Problem Statement Functionality

--- frontend/src/Pages/CreateProblem.jsx
+++ frontend/src/Pages/CreateProblem.jsx
@@ -1,19 +1,58 @@
-import { Select, Textarea, TextInput, Label, Button } from 'flowbite-react';
-import React from 'react';
+import { Select, Textarea, TextInput, Label, Button, Alert } from 'flowbite-react';
+import { useNavigate } from 'react-router-dom';
+import React, { useState } from 'react';
 
 export default function CreateProblem() {
+  const [formData, setFormData] = useState({});
+  const [submitError, setSubmitError] = useState(null);
+
+  const navigate = useNavigate();
+  
+  // console.log(formData);
+
+  const handleSubmit = async (e) => {
+    e.preventDefault();
+    try {
+      const res = await fetch('/api/problem/create', {
+        method: 'POST',
+        headers: {
+          'Content-Type': 'application/json',
+        },
+        body: JSON.stringify(formData),
+      });
+      const data = await res.json();
+      if (!res.ok) {
+        setSubmitError(data.message);
+        return;
+      }
+      if (res.ok) {
+        setSubmitError(null);
+        navigate('/problem-list');
+      }
+    } catch (error) {
+      setSubmitError('Something Went Wrong!');
+    }
+  };
   return (
     <div className='p-3 max-w-3xl mx-auto min-h-screen'>
       <h1 className='text-center text-3xl my-7 font-semibold'>Create a New Problem Statement</h1>
-      <form className='flex flex-col gap-4'>
+      <form className='flex flex-col gap-4' onSubmit={handleSubmit}>
         <div className='flex flex-col gap-4 justify-between'>
           <div>
             <Label htmlFor='title'>Title:</Label>
-            <Textarea type='text' placeholder='Title' required id='title' className='flex-1' rows={3}/>
+            <Textarea type='text' 
+            placeholder='Title' 
+            required id='title' 
+            className='flex-1' 
+            rows={3}
+            onChange={(e) =>
+              setFormData({ ...formData, title: e.target.value })
+            }
+            />
           </div>
           <div>
             <Label htmlFor='difficulty'>Difficulty:</Label>
-            <Select id='difficulty'>
+            <Select id='difficulty'  onChange={(e) => setFormData({ ...formData, difficulty: e.target.value })}>
               <option value='uncategorized'>Select Difficulty</option>
               <option value='easy'>Easy</option>
               <option value='medium'>Medium</option>
@@ -31,6 +70,9 @@ export default function CreateProblem() {
               id='description' 
               className='flex-1' 
               rows={8} 
+              onChange={(e) =>
+              setFormData({ ...formData, description: e.target.value })
+              }
             />
           </div>
           <div>
@@ -41,7 +83,10 @@ export default function CreateProblem() {
               required 
               id='inputformat' 
               className='flex-1' 
-              rows={6} 
+              rows={6}
+              onChange={(e) =>
+              setFormData({ ...formData, inputformat: e.target.value })
+              } 
             />
           </div>
           <div>
@@ -52,7 +97,10 @@ export default function CreateProblem() {
               required 
               id='outputformat' 
               className='flex-1' 
-              rows={6} 
+              rows={6}
+              onChange={(e) =>
+              setFormData({ ...formData, outputformat: e.target.value })
+              } 
             />
           </div>
         </div>
@@ -67,6 +115,9 @@ export default function CreateProblem() {
               id='input' 
               className='flex-1' 
               rows={3} 
+              onChange={(e) =>
+              setFormData({ ...formData, input: e.target.value })
+              }
             />
           </div>
         <div>
@@ -78,6 +129,9 @@ export default function CreateProblem() {
               id='inputvalue' 
               className='flex-1' 
               rows={3} 
+              onChange={(e) =>
+              setFormData({ ...formData, inputvalue: e.target.value })
+              }
             />
           </div>
         <div>
@@ -89,6 +143,9 @@ export default function CreateProblem() {
               id='output' 
               className='flex-1' 
               rows={3} 
+              onChange={(e) =>
+              setFormData({ ...formData, output: e.target.value })
+              }
             />
           </div>
         <div>
@@ -100,14 +157,20 @@ export default function CreateProblem() {
               id='explanation' 
               className='flex-1' 
               rows={4} 
+              onChange={(e) =>
+              setFormData({ ...formData, explanation: e.target.value })
+              }
             />
           </div>
           <div className="flex flex-row justify-between">
-          <Button gradientMonochrome="teal">Add Test Case</Button>
-          <Button gradientMonochrome="teal">Remove Test Case</Button>
+          <Button type='button' gradientMonochrome="teal">Add Test Case</Button>
+          <Button type='button' gradientMonochrome="teal">Remove Test Case</Button>
           </div>
         </div>
         <Button type='submit' gradientMonochrome="purple">Add Problem</Button>
+        {
+          submitError && <Alert className='mt-5' color='failure'>{submitError}</Alert>
+        }
       </form>
     </div>
   )
